d8a568ac7200a7fa589d932271a25ff2746117a7
Wen Li <wenlilearn@gmail.com>
2017-09-23 17:42:35 -0400

    Update mix.exs and LICENSE.md

--- mix.exs
+++ mix.exs
@@ -6,9 +6,26 @@ defmodule Mixeur.Mixfile do
       app: :mixeur,
       version: "0.1.0",
       elixir: "~> 1.5",
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/wli0503/Mixeur",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
+  end
+
+  defp package() do
+    [
+      name: "Mixeur",
+      maintainers: ["Wen Li"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://hex.pm/docs/publish"}
+    ]
+  end
+
+  defp description() do
+    "Mixin for Elixir"
   end
 
   # Run "mix help compile.app" to learn about applications.
